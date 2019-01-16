c12f982614130223945e15c277e58bc4d5ebecab
DeepisBeepis <Devilofawesomeness@gmail.com>
2019-01-16 08:32:56 -0500

    Created frame steps in training
    Polling inputs within each cycle

--- trenk/Assets/Scripts/Train/TrainGameStarter.cs
+++ trenk/Assets/Scripts/Train/TrainGameStarter.cs
@@ -9,16 +9,36 @@ public class TrainGameStarter : MonoBehaviour
     public GameEvent onRoundPrepare;
     public int arenaHeight = 30;
 
+    //  Grid space markers
+    [HideInInspector] public const byte EMPTY = 0;
+    [HideInInspector] public const byte P1 = 1;
+    [HideInInspector] public const byte P2 = 2;
+    [HideInInspector] public const byte HAZARD = 3;
+
+    // Indicate which direction each player is travelling
+    [HideInInspector] public const byte UP = 0;
+    [HideInInspector] public const byte RIGHT = 1;
+    [HideInInspector] public const byte DOWN = 2;
+    [HideInInspector] public const byte LEFT = 3;
+
+    private byte[,] board; // Stores positional data in arena
+    public byte[,] Board { get { return board; } }
+
     private GameObject homePlayer;
 
     void Start()
     {
         // Initialize underlying arena
-        GameBoard.Reset(arenaHeight);
+        board = new byte[arenaHeight, arenaHeight];
 
-        // Ready physical arena
+        // Ready board and physical arena
         for (int i = 1; i < arenaHeight - 1; i++)
         {
+            board[i, 0] = HAZARD;
+            board[0, i] = HAZARD;
+            board[i, arenaHeight - 1] = HAZARD;
+            board[arenaHeight - 1, i] = HAZARD;
+
             GameObject.Instantiate(fencePrefab, new Vector3(i, 0, 0), Quaternion.identity);
             GameObject.Instantiate(fencePrefab, new Vector3(0, 0, i), Quaternion.identity);
             GameObject.Instantiate(fencePrefab, new Vector3(i, 0, arenaHeight - 1), Quaternion.identity);
@@ -27,6 +47,7 @@ public class TrainGameStarter : MonoBehaviour
 
         // Initialize player
         homePlayer = Instantiate(playerPrefab);
+        homePlayer.transform.position = new Vector3((int)(arenaHeight / 3), 0, (int)(arenaHeight / 2));
         // Commence round readying
         onRoundPrepare.Raise();
     }
--- trenk/Assets/Scripts/Train/TrainRoundManager.cs
+++ trenk/Assets/Scripts/Train/TrainRoundManager.cs
@@ -2,18 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(TrainGameStarter))]
 public class TrainRoundManager : MonoBehaviour
 {
+    public int frameStep = 5;
 
-    // Start is called before the first frame update
-    void Start()
+    private byte[,] board;
+    private int gameFrame;
+    private int cycleFrame;
+    private bool turnChosen, requestLeft, requestRight;
+
+    private void Start()
     {
-        
+        board = GetComponent<TrainGameStarter>().Board;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        // Wait for player inputs for some frames
+        if (cycleFrame < frameStep)
+        {
+            cycleFrame++; // Increment cycle progress
+
+            // If an input hasn't been made this cycle...
+            if (!turnChosen)
+            {
+                // Poll inputs
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    requestLeft = turnChosen = true;
+                }
+                else if (Input.GetKeyDown(KeyCode.D))
+                {
+                    requestRight = turnChosen = true;
+                }
+            }
+        }
+        else
+        {
+            // Change world transforms
+            if (requestLeft)
+                Debug.Log("l");
+            else if (requestRight)
+                Debug.Log("r");
+            else
+                Debug.Log("_");
+
+            cycleFrame = 0; // Reset cycle progress
+            // Reset input polling
+            turnChosen = requestLeft = requestRight = false;
+        }
+
+        // Increment overall frame counter
+        gameFrame++;
     }
 }
