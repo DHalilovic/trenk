f42af47ee097db48600bfcc63fe0d26b8ddd75a4
DeepisBeepis <Devilofawesomeness@gmail.com>
2019-01-15 22:11:14 -0500

    Added training mode
    Preparing underlying grid system

---
+++ trenk/Assets/Scripts/Train/TrainGameStarter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainGameStarter : MonoBehaviour
+{
+    public GameObject playerPrefab; // Template GameObject for players
+    public GameObject fencePrefab;
+    public GameEvent onRoundPrepare;
+    public int arenaHeight = 30;
+
+    private GameObject homePlayer;
+
+    void Start()
+    {
+        // Initialize underlying arena
+        GameBoard.Reset(arenaHeight);
+
+        // Ready physical arena
+        for (int i = 1; i < arenaHeight - 1; i++)
+        {
+            GameObject.Instantiate(fencePrefab, new Vector3(i, 0, 0), Quaternion.identity);
+            GameObject.Instantiate(fencePrefab, new Vector3(0, 0, i), Quaternion.identity);
+            GameObject.Instantiate(fencePrefab, new Vector3(i, 0, arenaHeight - 1), Quaternion.identity);
+            GameObject.Instantiate(fencePrefab, new Vector3(arenaHeight - 1, 0, i), Quaternion.identity);
+        }
+
+        // Initialize player
+        homePlayer = Instantiate(playerPrefab);
+        // Commence round readying
+        onRoundPrepare.Raise();
+    }
+}
