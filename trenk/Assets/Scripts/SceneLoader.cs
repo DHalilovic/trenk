d07db5705560427e258de9e0daa0fc43a6ae8b70
Elijah <enavarro9696@yahoo.com>
2019-01-18 15:27:06 -0500

    more server testing

---
+++ trenk/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehavior
+{
+
+    public void LoadScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+}
