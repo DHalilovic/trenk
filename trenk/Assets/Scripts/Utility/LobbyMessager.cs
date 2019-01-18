609afb7f3a7713e938a085d219b89f36b1172ddb
Elijah <enavarro9696@yahoo.com>
2019-01-18 15:25:16 -0500

    Created server connection

---
+++ trenk/Assets/Scripts/Utility/LobbyMessager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyMessager : MonoBehaviour
+{
+    public string serverUrl;
+
+    public void GetHost()
+    {
+
+    }
+
+    public void AddSelfHost()
+    {
+        StartCoroutine(AddSelfHostCo());
+    }
+
+    IEnumerator AddSelfHostCo()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Post(serverUrl, new WWWForm()))
+        {
+            www.timeout = 1;
+            Debug.Log("ABOUT TO SEND");
+            yield return www.SendWebRequest();
+            Debug.Log("SENT");
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Posted");
+            }
+        }
+    }
+
+    public void RemoveSelfHost()
+    {
+
+    }
+}
