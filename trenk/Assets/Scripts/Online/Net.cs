f816458fc15c143d5520d9477c63c5d9bed41329
DeepisBeepis <Devilofawesomeness@gmail.com>
2019-01-22 14:49:51 -0500

    Updated Net
    Assimilated Client into Net, giving Net dual roles as Server/client depending on whether player is host. Must implement sending, receiving data.

--- trenk/Assets/Scripts/Online/Net.cs
+++ trenk/Assets/Scripts/Online/Net.cs
@@ -4,28 +4,33 @@ using System.Net.Sockets;
 
 public class Net
 {
-    private INetSerializer serializer; // Parses bytestreams.
+    public readonly short localPort = 8080;
+    public readonly short targetPort = 8080;
+
+    private INetSerializer serializer; // Parses bytestreams
+    private readonly bool server; // Is this user a server?
+    private readonly string targetIp; // Opponent's address
     private Socket listenerSocket; // Listens for connection requests
     private Socket clientSocket; // Opponent endpoint
-    private NetworkStream stream;
-    private bool server; // Is this user a server?
-    private string clientIp; // Opponent's address
+    private NetworkStream stream; // Wraps socket for data retrieval
     private readonly byte[] readBuffer = new byte[5000];
 
-    public Net(INetSerializer serializer, string clientIp)
+    public Net(INetSerializer serializer, bool server, string targetIp)
     {
         this.serializer = serializer;
+        this.server = server;
+        this.targetIp = targetIp;
     }
 
     public void Listen(int port)
     {
         IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-        IPAddress ipAddress = ipHostInfo.AddressList[0];
-        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+        IPAddress localIp = ipHostInfo.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(localIp, localPort);
 
         if (server)
         {
-            listenerSocket = new Socket(ipAddress.AddressFamily,
+            listenerSocket = new Socket(localIp.AddressFamily,
                 SocketType.Stream, ProtocolType.Tcp);
             listenerSocket.Bind(localEndPoint);
             listenerSocket.Listen(100);
@@ -33,7 +38,9 @@ public class Net
         }
         else
         {
-
+            clientSocket = new Socket(localIp.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.BeginConnect(new IPEndPoint(IPAddress.Parse(targetIp), targetPort), OnEndConnect, null);
         }
     }
 
@@ -42,23 +49,39 @@ public class Net
         Socket temp = listenerSocket.EndAccept(ar);
 
         // Accept only client provided by matchmaking
-        if (temp.RemoteEndPoint.ToString() == clientIp)
+        if (temp.RemoteEndPoint.ToString() == targetIp)
         {
             clientSocket = temp;
+            clientSocket.NoDelay = true; // Improve performance
             stream = new NetworkStream(clientSocket);
         }
+        else
+        {
+            temp.Close();
+        }
+    }
+
+    private void OnEndConnect(IAsyncResult ar)
+    {
+        clientSocket.EndConnect(ar);
     }
 
     public void OnDisconnect(IAsyncResult ar)
     {
+        stream.Close();
         clientSocket.Close();
         clientSocket = null;
     }
 
     public void Send(short type, int length, byte[] data)
     {
-
+        //stream.BeginWrite(new byte[] { (byte)(type >> 8) }, 0, 2, OnSend, null);
+        // Must send type, length first but still asynchronously
+        stream.BeginWrite(data, 0, data.Length, OnSend, null);
     }
 
+    public void OnSend(IAsyncResult ar)
+    {
 
+    }
 }
