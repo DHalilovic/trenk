c0614a36bbdf7c70b04e070f2d14dec69c8a0681
DeepisBeepis <Devilofawesomeness@gmail.com>
2019-01-21 19:25:06 -0500

    Implementing P2P
    Merging Client operations into Net for simplicity, given only two players may connect to one another per session.

---
+++ trenk/Assets/Scripts/Online/Client.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Sockets;
+
+public class Client
+{
+    private readonly Net net;
+    private readonly Socket clientSocket;
+    private readonly NetworkStream stream;
+    private readonly byte[] readBuffer = new byte[5000];
+
+    public Client(Net net, bool host)
+    {
+        this.net = net;
+        //this.clientSocket = net.ClientSocket;
+        this.clientSocket.NoDelay = true;
+        this.stream = new NetworkStream(this.clientSocket);
+
+        if (host)
+            // Client awaiting EndConnect
+            stream.BeginRead(readBuffer, 0, readBuffer.Length, OnRead, null);
+    }
+
+    public void close()
+    {
+        stream.Close();
+        clientSocket.Close();
+    }
+
+    private void OnRead(IAsyncResult ar)
+    {
+        int length = stream.EndRead(ar);
+
+        if (length <= 0)
+            net.OnDisconnect(ar);
+        else
+        {
+            // Process message
+            // Prepare to read more
+            stream.BeginRead(readBuffer, 0, readBuffer.Length, OnRead, null);
+        }
+    }
+}
---
+++ trenk/Assets/Scripts/Online/Net.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class Net
+{
+    private INetSerializer serializer; // Parses bytestreams.
+    private Socket listenerSocket; // Listens for connection requests
+    private Socket clientSocket; // Opponent endpoint
+    private NetworkStream stream;
+    private bool server; // Is this user a server?
+    private string clientIp; // Opponent's address
+    private readonly byte[] readBuffer = new byte[5000];
+
+    public Net(INetSerializer serializer, string clientIp)
+    {
+        this.serializer = serializer;
+    }
+
+    public void Listen(int port)
+    {
+        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ipAddress = ipHostInfo.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        if (server)
+        {
+            listenerSocket = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+            listenerSocket.Bind(localEndPoint);
+            listenerSocket.Listen(100);
+            listenerSocket.BeginAccept(OnListenerConnect, null);
+        }
+        else
+        {
+
+        }
+    }
+
+    protected void OnListenerConnect(IAsyncResult ar)
+    {
+        Socket temp = listenerSocket.EndAccept(ar);
+
+        // Accept only client provided by matchmaking
+        if (temp.RemoteEndPoint.ToString() == clientIp)
+        {
+            clientSocket = temp;
+            stream = new NetworkStream(clientSocket);
+        }
+    }
+
+    public void OnDisconnect(IAsyncResult ar)
+    {
+        clientSocket.Close();
+        clientSocket = null;
+    }
+
+    public void Send(short type, int length, byte[] data)
+    {
+
+    }
+
+
+}
